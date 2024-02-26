1eae105510150ff5b118baf50815aaf901911a68
Jeel Khant <141222261+jeelkhant3333@users.noreply.github.com>
2024-02-27 00:18:08 +0530

    initial commit

---
+++ lib/models/category.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+enum Categories{
+  vegetables,
+  fruit,
+  chocolate,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other,
+}
+
+class Category{
+  const Category(this.title,this.color);
+  final String title;
+  final Color color;
+}
